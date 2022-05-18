54296ada22a679df32866e7221f6abe6d501a7b2
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-18 19:31:15 +0600

    inventory:id add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -17,11 +17,18 @@ async function run() {
     try {
         await client.connect();
         const storeCollection = client.db("computer-warehouse").collection("store");
-        app.get("/store", async (req, res) => {
+        app.get("/inventory", async (req, res) => {
             const query = {};
             const cursor = storeCollection.find(query);
             const stores = await cursor.toArray();
             res.send(stores);
+        })
+
+        app.get("/inventory/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const store = await storeCollection.findOne(query);
+            res.send(store);
         })
     }
     finally {
